88f0c2cdf79e88d56c71c98d4aa3e01b585e192b
spiritualized <spiritualized>
2020-05-17 15:24:12 -0400

    Use library urldecode

--- api_v1.py
+++ api_v1.py
@@ -12,20 +12,18 @@ api_v1 = Blueprint('api', __name__)
 @inject
 @api_v1.route('/artists/<artist>')
 def get_artist(lastfmcache: LastfmCache, artist: str):
-    artist = artist.replace('∕', '/')
+    artist = LastfmCache.api_urldecode(artist)
     try:
-        artist_obj = lastfmcache.get_artist(artist)
-        return jsonpickle.encode(artist_obj, unpicklable=False)
+        return jsonpickle.encode(lastfmcache.get_artist(artist), unpicklable=False)
     except LastfmCache.ArtistNotFoundError:
         return "Artist not found: '{0}'".format(artist), 404
 
 @inject
 @api_v1.route('/artists/<artist>/releases/<release>')
 def get_release(lastfmcache: LastfmCache, artist: str, release: str):
-    artist = artist.replace('∕', '/')
-    release = release.replace('∕', '/')
+    artist = LastfmCache.api_urldecode(artist)
+    release = LastfmCache.api_urldecode(release)
     try:
-        release_obj = lastfmcache.get_release(artist, release)
-        return jsonpickle.encode(release_obj, unpicklable=False)
+        return jsonpickle.encode(lastfmcache.get_release(artist, release), unpicklable=False)
     except LastfmCache.ReleaseNotFoundError:
         return "Release not found: '{0}' by '{1}'".format(artist, release), 404
